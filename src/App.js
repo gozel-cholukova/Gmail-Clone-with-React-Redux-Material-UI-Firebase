fd65899300a75766b051e53f2d357120f5504468
Gozel Cholukova <gozelr@mail.ru>
2021-05-01 01:01:51 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Sidebar />
+      
+    </div>
+  );
+}
+
+export default App;
