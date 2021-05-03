13794cf78855fc7c45f425333ade361c65ff5aa8
Gozel Cholukova <gozelr@mail.ru>
2021-05-03 23:53:52 +0500

    Add files via upload

---
+++ src/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import SidebarOption from "./SidebarOption";
+import './Sidebar.css';
+import AddIcon from '@material-ui/icons/Add';
+import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <Button 
+        startIcon={<AddIcon fontSize="large" />}
+        className="sidebar__compose">
+        Compose
+      </Button>
+
+      <SidebarOption Icon={InboxIcon} title="Inbox" number={54}/>
+      <SidebarOption Icon={StarIcon} title="Starred" number={54}/>
+    </div>
+  );
+}
+
+export default Sidebar
