54e92fe088248dc0cb9f09da50f4f96ec4c212c0
Gozel Cholukova <gozelr@mail.ru>
2021-05-05 16:28:49 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Mail from "./Mail";
+import EmailList from "./EmailList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
+    <Router>
+      <div className="app">
+        <Header />
 
-      <Sidebar />
-      
-    </div>
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div> 
+      </div>
+    </Router>    
   );
 }
 
