448f61de5ea537ed35f7117f53946099b36a99ff
Gozel Cholukova <gozelr@mail.ru>
2021-05-11 18:18:04 +0500

    Add files via upload

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import "./EmailList.css";
+import EmailRow from "./EmailRow";
 import { Checkbox, IconButton  } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
-import SettingsIcon from '@material-ui/icons/Settings';
 import InboxIcon from '@material-ui/icons/Inbox';
 import PeopleIcon from '@material-ui/icons/People';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import Section from './Section';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 function EmailList() {
   return (
@@ -34,19 +33,11 @@ function EmailList() {
 
         <div className="emailList__settingsRight">
           <IconButton>
-            <ArrowLeftIcon />
+            <ChevronLeftIcon fontSize="small"/>
           </IconButton>
 
           <IconButton>
-            <ArrowRightIcon />
-          </IconButton>
-
-          <IconButton>
-            <KeyboardHideIcon />
-          </IconButton>
-
-          <IconButton>
-            <SettingsIcon />
+            <ChevronRightIcon fontSize="small"/>
           </IconButton>
         </div>
       </div>
@@ -55,6 +46,24 @@ function EmailList() {
         <Section Icon={InboxIcon} title="Primary" color="red" selected />
         <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
         <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+
+      <div className="emailList__list">
+        <EmailRow 
+          title="Boom"
+          subject="hey yo"
+          description="test"
+          time="2am"
+        />
+
+        <EmailRow 
+          title="Boom"
+          subject="hey yo"
+          description="test"
+          time="2am"
+        />
+
+        
       </div>
     </div>
   );
